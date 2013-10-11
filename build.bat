fc36a5a9070b6c46c671ac6d759c8f1e8f8b41f6
gro-ove <smpcsht@yahoo.com>
2013-10-12 00:46:34 +0400

    Build configuration updated (no compression now)

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 echo . Started
-cmd /C js-ext.cmd "%~dp0\src\js-ext.jsx" -o "%~dp0\build\js-ext.js" && echo . Converted || echo . Converting failed
-pegjs.cmd --cache < "%~dp0\src\js-ext.pegjs" | node -e "i='',q=String.fromCharCode(34),s=process.stdin;s.on('data',function(a){i+=a});s.on('end',function(){console.log('exports.parser='+i.replace ('return '+q+'Expected '+q+' + expectedHumanized','return '+q+'['+q+' + line + '+q+': '+q+' + column + '+q+'] '+q+' + '+q+'Expected '+q+' + expectedHumanized'))})" > "%~dp0\build\js-ext.parser" && echo . Parser builded || echo . Parser building failed
+cmd /C js-ext.cmd "%~dp0\src\js-ext.jsx" -o "%~dp0\build\js-ext.js" -e && echo . Converted || echo . Converting failed
+:: pegjs.cmd --cache < "%~dp0\src\js-ext.pegjs" | node -e "i='',q=String.fromCharCode(34),s=process.stdin;s.on('data',function(a){i+=a});s.on('end',function(){console.log('exports.parser='+i.replace ('return '+q+'Expected '+q+' + expectedHumanized','return '+q+'['+q+' + line + '+q+': '+q+' + column + '+q+'] '+q+' + '+q+'Expected '+q+' + expectedHumanized'))})" > "%~dp0\build\js-ext.parser" && echo . Parser builded || echo . Parser building failed
 :: uglifyjs2 "%~dp0\build\js-ext.parser" -c -o "%~dp0\build\js-ext.parser"
 :: echo . Parser minified
 echo . Finished
