f91775d2066968d756a6447cc2e93234aa481984
gro-ove <smpcsht@yahoo.com>
2013-12-06 12:25:05 +0400

    Build script rebuilded

--- build.js
+++ build.js
@@ -51,7 +51,7 @@ console.log (".. Error: " + stderr);
 else
 {
 console.log (".. Ok.");
-next (stdout);
+next ("Array.prototype.__defineGetter__(\"$\",function (){return this.map(function(a){return a instanceof Array?a.$:a}).join(\"\")});\n" + stdout);
 }
 });
 },". Parser saving:",function (arg){
