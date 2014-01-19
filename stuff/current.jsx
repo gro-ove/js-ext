a04c5bec890112c878c0cc30b06df4c91143f0f3
gro-ove <smpcsht@yahoo.com>
2014-01-20 02:39:12 +0400

    And new target - long strings

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,14 +1,2 @@
-var a = lambda (test){ result },
-	b = lambda { result },
-	c = lambda result,
-	d = lambda if (a){ b, c },
-	e = lambda a + b;
-
-var f = lambda {
-		a: a,
-		b: b
-	};
-
-var a = [ lambda { a: b }, lambda b ];
-
-[ lambda if (t){ a } else b, lambda if (q) return b ]
+var string = `test:
+				very lo\ng and multiline`;
