acedf3a526af7c535eb674c90f6fb40654dc1c69
gro-ove <smpcsht@yahoo.com>
2014-01-20 20:59:58 +0400

    Now with vars

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,12 +1,12 @@
-for (var KEY in OBJECT){ OPERATION } 
-for (var KEY, VALUE in OBJECT){ OPERATION }
+// for (var KEY in OBJECT){ OPERATION } 
+// for (var KEY, VALUE in OBJECT){ OPERATION }
 
-for (KEY in OBJECT){ OPERATION } 
-for (KEY, VALUE in OBJECT){ OPERATION }
+// for (KEY in OBJECT){ OPERATION } 
+// for (KEY, VALUE in OBJECT){ OPERATION }
 
-for (KEY, VALUE in OBJECT) OPERATION;
+// for (var KEY, VALUE in OBJECT) OPERATION;
+// for (KEY, VALUE in OBJECT) OPERATION;
 
-for (var KEY in OBJECT){ var VALUE = OBJECT [KEY] } 
+// for (var KEY in OBJECT){ var VALUE = OBJECT [KEY] } 
 
-//for (var n in-array a){}
-//for (var n, v in-array a){}
+for (var KEY, VALUE in COMPLEX_OBJECT ()){ OPERATION }
