4f2cdd54663979a51619559ca1083bd388a2cda1
gro-ove <smpcsht@yahoo.com>
2014-01-21 03:44:32 +0400

    And let's moving on :)

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,9 +1,3 @@
-// ==Jsx==
-// @key value
-// @flag
-// ==/Jsx==
-
-// EXIT
-
-function hi ()
-	console.log ('\u2028' 187 lambda 15 lambda 18 { a: 20 b: 25 c: [ lambda 14 lambda 'hi' ] });
+module A {
+	
+}
--- stuff/test.bat
+++ stuff/test.bat
@@ -1,10 +1,11 @@
 @echo off
 cmd /C js-ext.cmd src\js-ext.jsx -e -o build\js-ext.temp.js
-node build\js-ext.temp.js stuff\test-native.jsx -l 4059767
-node build\js-ext.temp.js stuff\test-lambda.jsx -l 17404
-node build\js-ext.temp.js stuff\test-strings.jsx -l 576
-node build\js-ext.temp.js stuff\test-arguments.jsx -l 4517
-node build\js-ext.temp.js stuff\test-for-in-loops.jsx -l 19794
-node build\js-ext.temp.js stuff\test-for-in-array-loops.jsx -l 13668
-node build\js-ext.temp.js stuff\test-try.jsx -l 9824
+node build\js-ext.temp.js stuff\test-native.jsx -l 4059818
+node build\js-ext.temp.js stuff\test-lambda.jsx -l 17455
+node build\js-ext.temp.js stuff\test-strings.jsx -l 627
+node build\js-ext.temp.js stuff\test-arguments.jsx -l 4568
+node build\js-ext.temp.js stuff\test-for-in-loops.jsx -l 19845
+node build\js-ext.temp.js stuff\test-for-in-array-loops.jsx -l 13719
+node build\js-ext.temp.js stuff\test-try.jsx -l 9875
+node build\js-ext.temp.js stuff\test-commafree.jsx -l 9476
 node build\js-ext.temp.js stuff\current.jsx
