3f816bcf5b45075b8bfebc5656bb3cb375ec3a04
gro-ove <smpcsht@yahoo.com>
2014-01-21 18:05:06 +0400

    Modules support is coming

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,3 +1,23 @@
 module A {
-	
+	var a = 18,
+		b = B.variable;
+
+	function init (){
+
+	}
 }
+
+__m ('A', function (){
+	var a = 18, b;
+
+	function init (){
+
+	}
+
+	return {
+		__s: function (){
+			b = B.variable;
+		},
+		__i: init
+	}
+});
