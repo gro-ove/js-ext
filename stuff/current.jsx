19f89a1e168dd5008635a7ea0cac0d5aa17a7bd8
gro-ove <smpcsht@yahoo.com>
2014-01-24 02:28:23 +0400

    Looks like I can remove "__m"

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,82 +1,48 @@
-{
-	A = function (arg){
-		this.local = arg;
-		this.pvar = 20;
-		this.provar = 25;
-		this.hi = 18;
-	};
-
-	(function (){
-		A.prototype.workThis = function (){}
-
-		var a = 18;
-
-		function localFunction (){
-			doSomethingLocal ();
-		}
-
-		function workAllDay (){
-			doSomething ();
-		}
-
-		A.workAllDay = workAllDay;
-	})();
-}
-
-/*class B {
-	static public var variable = JSON.stringify ({ key: 'value' });
-}
-
-class C {
-	static public function test (){
-		return 187;
-	}
-}
-
 class A {
-	use B, C;
+	// use B, C;
 
-	static var a = 18,
-		b = B.variable;
+	static var a = 18, b = test;
 
-	static function localFunction (){
-		doSomethingLocal ();
+	static function privateStatic (){
+		doSomethingLocal
 	}
 
-	static public function workAllDay (){
-		doSomething ();
+	static public function publicStatic (){
+		doSomething
 	}
 
 	static {
-		a = a * C.test ();
+		staticInitialize
 	}
 
-	public var local, temp, hi = 18;
+	public var publicVar = 1;
+	private var privateVar = 2;
+	protected var protectedVar = 3;
 
-	private var pvar = 20;
-	protected var provar = 25;
+	public function publicObject (){
+		protectedVar = protectedVar + 1;
 
-	public function workThis (){
-		hi = hi + 1;
+		var publicVar = function (){};
 
-		var pvar = function (){
-			temp = temp * pvar;
-		};
-
-		pvar ();
-
-		return {
-			hi: hi + 1,
-			pvar: pvar
-		}
+		publicVar ();
 	}
 
 	(arg){
-		local = arg;
+		privateVar = privateVar + arg;
 	}
 }
 
-class Child extends A {
+// class B {
+// 	static public var variable = JSON.stringify ({ key: 'value' });
+// }
+
+// class C {
+// 	static public function test (){
+// 		return 187;
+// 	}
+// }
+
+/*class Child extends A {
 	public function blaBlaBla (){
 		super ('QWERTY!');
 		local = 'LOCAL!' + provar;
@@ -95,53 +61,4 @@ class ChildChild extends Child {
 
 		workThis ();
 	}
-}
-
-*/
-// __m ('A', function (){
-// 	var a = 18, b;
-
-// 	function localFunction (){
-// 		doSomethingLocal ();
-// 	}
-
-// 	function workAllDay (){
-// 		doSomething ();
-// 	}
-
-// 	return {
-// 		workAllDay: workAllDay,
-// 		@s: function (){
-// 			b = B.variable;
-// 		},
-// 		@i: function (){
-// 			a = a * C.test ();
-// 		},
-// 		@c: {
-// 			@i: function (arg){
-// 				this.local = arg;
-// 				this.hi = 18;
-// 			},
-
-// 			workThis: function (){
-// 				this.hi = this.hi + 1;
-// 			}
-// 		}
-// 	}
-// }, [ 'B', 'C' ]);
-
-
-// __m ('A', function (){
-// 	var a = 18, b;
-
-// 	
-
-// 	return {
-// 		__s: function (){
-// 			b = B.variable;
-// 		},
-// 		__i: function (){
-
-// 		}
-// 	}
-// });
+}*/
