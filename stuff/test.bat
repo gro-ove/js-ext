99607eae43e9a62d7cbf2d1da27b6e74153b639d
gro-ove <smpcsht@yahoo.com>
2014-01-25 16:29:10 +0400

    Updated

--- stuff/test.bat
+++ stuff/test.bat
@@ -4,8 +4,8 @@ node build\js-ext.temp.js stuff\test-native.jsx -l 4059818 || exit /b
 node build\js-ext.temp.js stuff\test-lambda.jsx -l 17131 || exit /b
 node build\js-ext.temp.js stuff\test-strings.jsx -l 627 || exit /b
 node build\js-ext.temp.js stuff\test-arguments.jsx -l 4358 || exit /b
-node build\js-ext.temp.js stuff\test-for-in-loops.jsx -l 19845 || exit /b
-node build\js-ext.temp.js stuff\test-for-in-array-loops.jsx -l 13719 || exit /b
+node build\js-ext.temp.js stuff\test-for-in-loops.jsx -l 19821 || exit /b
+node build\js-ext.temp.js stuff\test-for-in-array-loops.jsx -l 13683 || exit /b
 node build\js-ext.temp.js stuff\test-try.jsx -l 9875 || exit /b
 :: node build\js-ext.temp.js stuff\test-commafree.jsx -l 9476 || exit /b
 node build\js-ext.temp.js stuff\current.jsx
