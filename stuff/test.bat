02148f9c91ba20b86e239a8324c71a874d5f83c7
gro-ove <smpcsht@yahoo.com>
2014-01-25 15:29:42 +0400

    Fixed and updated

--- stuff/test.bat
+++ stuff/test.bat
@@ -1,9 +1,9 @@
 @echo off
 cmd /C js-ext.cmd src\js-ext.jsx -e -o build\js-ext.temp.js || exit /b
 node build\js-ext.temp.js stuff\test-native.jsx -l 4059818 || exit /b
-node build\js-ext.temp.js stuff\test-lambda.jsx -l 17455 || exit /b
+node build\js-ext.temp.js stuff\test-lambda.jsx -l 17131 || exit /b
 node build\js-ext.temp.js stuff\test-strings.jsx -l 627 || exit /b
-node build\js-ext.temp.js stuff\test-arguments.jsx -l 5562 || exit /b
+node build\js-ext.temp.js stuff\test-arguments.jsx -l 4358 || exit /b
 node build\js-ext.temp.js stuff\test-for-in-loops.jsx -l 19845 || exit /b
 node build\js-ext.temp.js stuff\test-for-in-array-loops.jsx -l 13719 || exit /b
 node build\js-ext.temp.js stuff\test-try.jsx -l 9875 || exit /b
