781c98600aa9052cac4f82b88767eb3673234fd6
gro-ove <smpcsht@yahoo.com>
2014-01-28 00:59:51 +0400

    Shame on me!

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,9 +1,7 @@
 // ==Jsx==
-// @macros test '`%0`'(new Date)
-// @macros time '`%0`'(new Date (args [0]))
-// @macros json JSON.stringify (args)
+// @macro test '`%0`'(new Date)
+// @macro time '`%0`'(new Date (args [0]))
+// @macro json JSON.stringify (args)
 // ==/Jsx==
 
-console.log (@json (10525, 0.18, 016, 0x15, 18e3, 18e+3, 18e-3, 'te\'\nst', `te
-
-	st`, { a: 7, b: 8 }));
+ console.log (@json (10525, 0.18, 016, 0x15, 18e3, 18e+3, 18e-3, 'te\'\nst', `test`, '{ a: 7, b: 8 }'));
