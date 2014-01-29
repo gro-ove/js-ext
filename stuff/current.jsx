43ee4bc224004a4f6f1f225aebc2cb230f2a5234
gro-ove <smpcsht@yahoo.com>
2014-01-30 01:17:25 +0400

    Fixed

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -1,6 +1,13 @@
+// var constTest 			= @const,
+// 	numberArgTest 		= @argsTest (0),
+// 	stringArgTest 		= @argsTest ('string'),
+// 	objectArgTest 		= @argsTest ({ a: 7 }),
+// 	macroArgTest 		= @argsTest (@const);
+
 {
 	console.log (@const);
 	@macro const 25;
 }
 
 @macro const 20;
+// @macro argsTest JSON.stringify (arg);
