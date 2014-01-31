813f24e3310a81779f4eefc8ba765320ec22ea39
gro-ove <smpcsht@yahoo.com>
2014-02-01 03:25:00 +0400

    This is madness

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -5,12 +5,44 @@ var constTest 			= @const,
 	macroArgTest 		= @argsTest ({ constValue: @const }),
 	multiArgTest 		= @multiArgsTest (1, 2, 3);
 
-@macro const 20;
+var carOrDog = @enumMacro {
+	Cat, 
+	Dog, 
+	CatDog
+};
+
+var helloWorld = @crazyMacro {{
+		STRING:
+		H } e } l } l } o } # } w } o } r } l } d } !
+	}};
 
 {
-	console.log (@const);
+	console.log (@const, @argsTest ([ @const ]));
+	@crazyMacro {{ RAW: @ } c } o } n } s } t } # } /* } #IS IT EQUALS TO TWENTY FIVE?# } */ } }}
+
 	@macro const 25;
 }
 
 @macro argsTest JSON.stringify (arg);
+
 @macro multiArgsTest (a, b, c) JSON.stringify ([ a, b, c ]);
+
+@macro enumMacro (arg){
+	var result = {};
+
+	console.assert (typeof arg === 'string', 'Argument must be string');
+	arg.split (',').map (lambda (arg) arg.trim ()).forEach (lambda (arg) result [arg] = arg);
+
+	return {
+		type: 	MacroReturnType.Object,
+		value: 	result
+	};
+}
+
+@macro const 20;
+
+@macro crazyMacro (arg)
+	{
+		type: 	arg.indexOf ('STRING:') === -1 ? MacroReturnType.Raw : MacroReturnType.String,
+		value: 	arg.match (/[^:]+$/)[0].split ('}').map (lambda (arg) arg.trim ()).join ('').replace (/#/g, ' ')
+	};
