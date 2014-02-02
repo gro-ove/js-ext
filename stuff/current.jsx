9c7ed23d1bfd91732491fd8b9cec55e3b6115cca
gro-ove <smpcsht@yahoo.com>
2014-02-02 15:28:47 +0400

    Awesome!

--- stuff/current.jsx
+++ stuff/current.jsx
@@ -3,49 +3,51 @@
 @macro multiArgsTest (a, b, c) JSON.stringify ([ a, b, c ]);
 @macro areasTest '@const';
 
-var constTest 			= @const,
-	numberArgTest 		= @argsTest (0),
-	stringArgTest 		= @argsTest ('string'),
-	objectArgTest 		= @argsTest ({ a: 7 }),
-	macroArgTest 		= @argsTest ({ constValue: @const }),
-	multiArgTest 		= @multiArgsTest (1, 2, 3);
+var constTest           = @const,
+    numberArgTest       = @argsTest (0),
+    stringArgTest       = @argsTest ('string'),
+    objectArgTest       = @argsTest ({ a: 7 }),
+    macroArgTest        = @argsTest ({ constValue: @const }),
+    multiArgTest        = @multiArgsTest (1, 2, 3);
 
-@macro enumMacro (arg){
-	var result = {};
+@macro enumMacro (arg, callback){
+    var result = {};
 
-	console.assert (typeof arg === 'string', 'Argument must be string');
-	arg.split (',').map (lambda (arg) arg.trim ()).forEach (lambda (arg) result [arg] = arg);
+    console.assert (typeof arg === 'string', 'Argument must be string');
+    arg.split (',').map (lambda (arg) arg.trim ()).forEach (lambda (arg) result [arg] = arg);
 
-	return {
-		type: 	ReturnType.Object,
-		value: 	result
-	};
+    setTimeout (lambda {
+        callback ({
+            type:   ReturnType.Object,
+            value:  result
+        });
+    }, 200);
 }
 
 var carOrDog = @enumMacro {
-	Cat, 
-	Dog, 
-	CatDog
+    Cat, 
+    Dog, 
+    CatDog
 };
 
 @macro crazyMacro (arg)
-	{
-		type: 	arg.indexOf ('STRING:') === -1 ? ReturnType.Raw : ReturnType.String,
-		value: 	arg.match (/[^:]+$/)[0].split ('}').map (lambda (arg) arg.trim ()).join ('').replace (/#/g, ' ')
-	};
+    {
+        type:   arg.indexOf ('STRING:') === -1 ? ReturnType.Raw : ReturnType.String,
+        value:  arg.match (/[^:]+$/)[0].split ('}').map (lambda (arg) arg.trim ()).join ('').replace (/#/g, ' ')
+    };
 
 var helloWorld = @crazyMacro {{
-		STRING:
-		H } e } l } l } o } # } w } o } r } l } d } !
-	}};
+        STRING:
+        H } e } l } l } o } # } w } o } r } l } d } !
+    }};
 
 {
-	console.log (@const (), @argsTest ([ @const, @areasTest ]));
-	console.log (@macroWhichUsingAnotherMacro);
-	@crazyMacro {{ RAW: @ } c } o } n } s } t } # } /* } #IS IT EQUALS TO TWENTY FIVE?# } */ } }}
+    console.log (@const (), @argsTest ([ @const, @areasTest ]));
+    console.log (@macroWhichUsingAnotherMacro);
+    @crazyMacro {{ RAW: @ } c } o } n } s } t } # } /* } #IS IT PSEUDOCONST?# } */ } }}
 
-	@macro macroWhichUsingAnotherMacro (){
-		return @argsTest ({ a: 18, b: @const, c: @argsTest ([ 'test' ]) });
-	}
-	@macro const 25;
+    @macro macroWhichUsingAnotherMacro (){
+        return @argsTest ({ a: 18, b: @const, c: @argsTest ([ 'test' ]) });
+    }
+    @macro const (callback) setTimeout (lambda callback (+new Date % 10000), 200); // NOT A CONST AT ALL
 }
