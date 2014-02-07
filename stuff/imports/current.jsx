d02d2e84271195559120c1dfcf2ef71b6f80c198
gro-ove <smpcsht@yahoo.com>
2014-02-07 17:42:03 +0400

    A wild bug appears!

--- stuff/imports/current.jsx
+++ stuff/imports/current.jsx
@@ -1,15 +1,3 @@
-/*{
-	@macro param (key, value){
-		params.add (key, value);
-	}
-
-	@param ('import', 'other');
-}*/
-
-@macro time new Date ().getMinutes ();
-var current_t = @time;
-
-@macro arg 1000 + arg;
-var current_o = @arg (1);
-
-var current_to = @arg (@time);
+@test
+@macro test (callback)
+    setTimeout (lambda callback ('Hello World!'), 100);
