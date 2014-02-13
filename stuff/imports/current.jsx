10a045bcf2bcb59454272bfba1c578ac272309bb
gro-ove <smpcsht@yahoo.com>
2014-02-13 23:55:59 +0400

    Require added, names fixed, updated

--- stuff/imports/current.jsx
+++ stuff/imports/current.jsx
@@ -1,39 +1,60 @@
-@import ('other')
+// @import ('other')
 
-@macro time new Date ().getMinutes ();
-var current_t = @time;
-
-@macro arg 1000 + arg;
-var current_o = @arg (1);
-
-var current_to = @arg (@time);
-
-@macro file { type: ReturnType.String, value: context.file.fullpath };
-console.log (@file);
-
-var diffucultTest = @macroInOtherFile;
-
+// @macro time new Date ().getMinutes ();
 class A {
-	private var variable = 10;
-
-	public function test (){
-		return lambda arg + variable;
+	protected function fn (){
+		console.log ('A');
 	}
 
-	public function check (){
-		test ();
-		return test;
+	(){
+		fn ();
 	}
 }
 
-function oneLineWithResult (arg)
-	if (arg)
-		arg + 20;
+class B extends A {
+	protected function fn (){
+		console.log ('B');
+		super ();
+	}
+}
 
-function oneLineWithoutArgumentsWithResult 
-	if (arg)
-		arg + 20;
+{
+	new B ();
+}
 
-function callRightHere (){
-	new A
-}(current_o)
+// var current_t = @time;
+
+// @macro arg 1000 + arg;
+// var current_o = @arg (1);
+
+// var current_to = @arg (@time);
+
+// @macro file { type: ReturnType.String, value: context.file.fullpath };
+// console.log (@file);
+
+// var diffucultTest = @macroInOtherFile;
+
+// class A {
+// 	private var variable = 10;
+
+// 	public function test (){
+// 		return lambda arg + variable;
+// 	}
+
+// 	public function check (){
+// 		test ();
+// 		return test;
+// 	}
+// }
+
+// function oneLineWithResult (arg)
+// 	if (arg)
+// 		arg + 20;
+
+// function oneLineWithoutArgumentsWithResult 
+// 	if (arg)
+// 		arg + 20;
+
+// function callRightHere (){
+// 	new A
+// }(current_o)
