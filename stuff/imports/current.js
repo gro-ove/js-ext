19a4ff5e80fccccbc52e1efd75568301d94f3ac8
ls-4 <smpcsht@yahoo.com>
2014-03-09 18:23:19 +0400

    Fixed

--- stuff/imports/current.js
+++ stuff/imports/current.js
@@ -1,25 +1,24 @@
-function a (){                                  // current.jsx:1
-	'' + 5 + '';                                // current.jsx:2
-	'' + 5 + '' + 8 + '';                       // current.jsx:3
-	'test' + 5 + '' + 8 + '';                   // current.jsx:4
-	'test';                                     // current.jsx:5
-	'' + 5 + '-%' + 5 + '-' + 8 + '';           // current.jsx:6
-}
+var a = function (){                                // current.jsx:23
+		'' + 5 + '';                                // current.jsx:2
+		'' + 5 + '' + 8 + '';                       // current.jsx:3
+		'test' + 5 + '' + 8 + '';                   // current.jsx:4
+		'test';                                     // current.jsx:5
+		'' + 5 + '-%' + 5 + '-' + 8 + '';           // current.jsx:6
+	}, 
+	b = function (){                                // current.jsx:21
+		function q (){}                             // current.jsx:9
+		
+		'' + variable5 + '';                        // current.jsx:11
+		'' + variable5 + '' + variable8 + '';       // current.jsx:12
+		'test' + variable5 + '' + variable8 + '';   // current.jsx:13
+		
+		function a (){}                             // current.jsx:15
+		
+		function b (){}                             // current.jsx:17
+		
+		if (a)                                      // current.jsx:19
+			function s (){}                         // current.jsx:20
+	};
 
-'qwerty';                                       // current.jsx:9
-console.log ('test');                           // current.jsx:10
-
-function b (){                                  // current.jsx:12
-	function q (){}                             // current.jsx:13
-	
-	'' + variable5 + '';                        // current.jsx:15
-	'' + variable5 + '' + variable8 + '';       // current.jsx:16
-	'test' + variable5 + '' + variable8 + '';   // current.jsx:17
-	
-	function a (){}                             // current.jsx:19
-	
-	function b (){}                             // current.jsx:21
-	
-	if (a)                                      // current.jsx:23
-		function s (){}                         // current.jsx:24
-}
+'qwerty';                                           // current.jsx:23
+console.log ('test');
